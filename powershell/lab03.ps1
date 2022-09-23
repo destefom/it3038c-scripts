9439b2fa92af3785918891186e6999e59a120924
destefom <91615790+destefom@users.noreply.github.com>
2022-09-23 09:00:32 -0400

    Add files via upload

---
+++ powershell/lab03.ps1
@@ -0,0 +1,8 @@
+﻿$IP = getIP
+$USER = Get-LocalUser
+$Host = Get-Host
+$HOST = get-host
+$Date = Get-Date
+Write-Host("This machine's IP is $IP, User is $USER, Hostname is $Host, Today's date is $Date")
+$Body = Write-Host
+Send-MailMessage -To "reedws-win" -From "oliviadestefano20@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $Body -SmtpServer smtp.gmail.com -port 587 -UseSsl -Credential (Get-Credential)
