173075e41dcbf265775700ccb8ada5c2d732cb2d
destefom <91615790+destefom@users.noreply.github.com>
2022-09-23 09:00:55 -0400

    Add files via upload

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,10 @@
+﻿$Hello = "Hello PowerShell"
+Write-Host($Hello)
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+function getIP{
+    (Get-NetIPAddress).IPv4Address | select-string "192*"
+    }
+Write-Host(getIP)
+$IP = getIP
+Write-Host("This machine's IP is $IP")
+Write-Host("This machine's IP is {0}" -f $IP)
